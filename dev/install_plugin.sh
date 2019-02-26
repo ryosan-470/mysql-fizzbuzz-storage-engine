7fb31ebf0d7efb4b5499b010c9220afcf5a0b93a
ryosan-470 <rskjtwp@gmail.com>
2019-02-26 15:06:48 +0900

    fix install plugin syntax error

--- dev/install_plugin.sh
+++ dev/install_plugin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-STORAGE_ENGINES=(fizzbuzz")
+STORAGE_ENGINES=("fizzbuzz")
 for engine in ${STORAGE_ENGINES[@]}; do
     sql="INSTALL PLUGIN ${engine} SONAME \"ha_${engine}.so\""
     mysql -uroot -e "${sql}"
